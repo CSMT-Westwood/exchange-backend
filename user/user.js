d0cc64ee162e0e8d06e28ef4e902596e75e2b7cf
Yiyang <yiyangzhou123@ucla.edu>
2020-05-09 15:40:35 -0700

    Add a getUser by Username route

--- user/user.js
+++ user/user.js
@@ -1,37 +1,45 @@
 /*
 API for user info
 */
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const User = require('../models/User') //get model
+const User = require("../models/User"); //get model
 
-router.get('/', (req, res) => {
-    res.send("we are at /user/");
+router.get("/", (req, res) => {
+  res.send("we are at /user/");
 });
 
-
-router.post('/signup', (req,res)=>{
-    
-    
-    const newUser = new User({
-        username: req.body.username,
-        password: req.body.password,
-        email: req.body.email,
-        preferences: req.body.preferences,
-        info: req.body.info
+//find user by username.
+router.get("/:username", (req, res) => {
+  const users = User.find({ username: req.params.username }).exec();
+  users
+    .then((data) => {
+      res.json(data);
+    })
+    .catch((err) => {
+      res.json({ message: err });
     });
-    
-        newUser.save()
-               .then(data => {
-                   console.log(data);
-                   res.json(data);
-               })
-               .catch(err =>{
-                   res.json({message: err});
-               });
+});
 
-    
+router.post("/signup", (req, res) => {
+  const newUser = new User({
+    username: req.body.username,
+    password: req.body.password,
+    email: req.body.email,
+    preferences: req.body.preferences,
+    info: req.body.info,
+  });
+
+  newUser
+    .save()
+    .then((data) => {
+      console.log(data);
+      res.json(data);
+    })
+    .catch((err) => {
+      res.json({ message: err });
+    });
 });
 
 module.exports = router;
