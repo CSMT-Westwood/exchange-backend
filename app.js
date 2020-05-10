e5d56f5e09df3a303b44cb849363ae0ec327fd98
Yiyang <yiyangzhou123@ucla.edu>
2020-05-09 20:27:28 -0700

    Add login input validation
    Adjusted TAB format

--- app.js
+++ app.js
@@ -1,28 +1,28 @@
-const express = require('express');
-const mongoose = require('mongoose');
-require('dotenv/config');
+const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv/config");
 
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 const app = express();
-const user = require('./user/user');
+const user = require("./user/user");
 
 // middlewares
 app.use(bodyParser.json());
 
 // routing
-app.use('/user', user);
+app.use("/user", user);
 
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
     res.send("Hello, world!");
 });
 
 // suppress the warnings
-mongoose.set('useNewUrlParser', true);
-mongoose.set('useUnifiedTopology', true);
+mongoose.set("useNewUrlParser", true);
+mongoose.set("useUnifiedTopology", true);
 // connect to db
-mongoose.connect(process.env.DB_CONNECTION, ()=>{
+mongoose.connect(process.env.DB_CONNECTION, () => {
     console.log("connected to DB!");
-})
+});
 
 // listening on port 8000
 app.listen(8000);
--- user/user.js
+++ user/user.js
@@ -10,83 +10,91 @@ const bcrypt = require("bcryptjs");
 
 //For debugging purpose, return all users in database
 router.get("/", async (req, res) => {
-  const AllUsers = await User.find();
-  res.send(AllUsers);
+    const AllUsers = await User.find();
+    res.send(AllUsers);
 });
 
 //find user by username.
 router.get("/:username", (req, res) => {
-  const users = User.find({ username: req.params.username }).exec();
-  users
-    .then((data) => {
-      res.json(data);
-    })
-    .catch((err) => {
-      res.json({ message: err });
-    });
+    const users = User.find({ username: req.params.username }).exec();
+    users
+        .then((data) => {
+            res.json(data);
+        })
+        .catch((err) => {
+            res.json({ message: err });
+        });
 });
 
 /******User Signup API******/
 router.post("/signup", async (req, res) => {
-  const newUser = new User({
-    username: req.body.username,
-    password: req.body.password,
-    email: req.body.email,
-    preferences: req.body.preferences,
-    info: req.body.info,
-  });
-  const error = validations.signUpSchema.validate(req.body).error;
-  if (error) {
-    return res.status(400).send(error.details[0].message); //send message if input is invalid
-  }
-  //Check duplicates in database
-  if (await User.findOne({ username: req.body.username }))
-    return res.status(400).send("The username already exists.");
-  if (await User.findOne({ email: req.body.email }))
-    return res.status(400).send("The email has been registered.");
+    const newUser = new User({
+        username: req.body.username,
+        password: req.body.password,
+        email: req.body.email,
+        preferences: req.body.preferences,
+        info: req.body.info,
+    });
+    const error = validations.signUpSchema.validate(req.body).error;
+    if (error) {
+        return res.status(400).send(error.details[0].message); //send message if input is invalid
+    }
+    //Check duplicates in database
+    if (await User.findOne({ username: req.body.username }))
+        return res.status(400).send("The username already exists.");
+    if (await User.findOne({ email: req.body.email }))
+        return res.status(400).send("The email has been registered.");
 
-  //Hash Password
-  const salt = await bcrypt.genSalt();
-  const hashedpassword = await bcrypt.hash(req.body.password, salt);
-  newUser.password = hashedpassword;
+    //Hash Password
+    const salt = await bcrypt.genSalt();
+    const hashedpassword = await bcrypt.hash(req.body.password, salt);
+    newUser.password = hashedpassword;
 
-  //Creat a new user
-  try {
-    const addedUser = await newUser.save();
-    res.status(200).send(addedUser);
-  } catch (err) {
-    res.json({ message: err });
-  }
+    //Creat a new user
+    try {
+        const addedUser = await newUser.save();
+        res.status(200).send(addedUser);
+    } catch (err) {
+        res.json({ message: err });
+    }
 });
 
 //*****User Login API******/
 //body params: name(Email OR Username), password
 router.post("/login", async (req, res) => {
-  let UserbyName = await User.findOne({ email: req.body.name }); //try email
-  if (!UserbyName) {
-    UserbyName = await User.findOne({ username: req.body.name }); //try username
-    if (!UserbyName) return res.status(400).send("incorrect, try again."); //if not found, return error
-  }
+    //check format thru validation
+    const error = validations.loginSchema.validate(req.body).error;
+    if (error) {
+        return res.status(400).send(error.details[0].message);
+    }
 
-  //check password
-  const rightPassword = await bcrypt.compare(
-    req.body.password,
-    UserbyName.password
-  );
-  if (!rightPassword) res.status(400).send("incorrect, try again.");
+    let UserbyName = await User.findOne({ email: req.body.name }); //try email
+    if (!UserbyName) {
+        UserbyName = await User.findOne({ username: req.body.name }); //try username
+        if (!UserbyName) return res.status(400).send("incorrect, try again."); //if not found, return error
+    }
 
-  //******TODO*********//
-  res.status(200).json({ message: "success" });
+    //check password
+    const rightPassword = await bcrypt.compare(
+        req.body.password,
+        UserbyName.password
+    );
+    if (!rightPassword) return res.status(400).send("incorrect, try again.");
+
+    //******TODO*********//
+    res.status(200).json({ message: "success" });
 });
 
-//delete user by username API
+//delete all user by username API, for debugging purposes
 router.delete("/deleteUser/:username", async (req, res) => {
-  try {
-    const removedUser = await User.remove({ username: req.params.username });
-    res.json(removedUser);
-  } catch (err) {
-    res.json({ message: err });
-  }
+    try {
+        const removedUser = await User.remove({
+            username: req.params.username,
+        });
+        res.json(removedUser);
+    } catch (err) {
+        res.json({ message: err });
+    }
 });
 
 module.exports = router;
