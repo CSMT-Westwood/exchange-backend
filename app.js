cb5023102da7f7ea305a1cbb633b8048a7b5d277
Tony Xia <summertony717@gmail.com>
2020-05-10 15:12:02 -0700

    enabled CORS

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
 require("dotenv/config");
 
 const bodyParser = require("body-parser");
@@ -8,6 +9,7 @@ const user = require("./user/user");
 
 // middlewares
 app.use(bodyParser.json());
+app.use(cors());
 
 // routing
 app.use("/user", user);
