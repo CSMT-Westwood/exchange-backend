b9421ca76a7748bc5b3e30bf96b658f7cdec1b7d
Yiyang <yiyangzhou123@ucla.edu>
2020-05-16 22:52:30 -0700

    organized code, added a toolkit for image, added error handling for image upload

--- avatar.js
+++ avatar.js
@@ -1,57 +1,36 @@
 const express = require("express");
 const router = express.Router();
-//const parser = require("./image");
+const image = require("./image");
 const cloudinary = require("cloudinary");
 const multer = require("multer");
 const DatauriParser = require("datauri/parser");
 const path = require("path");
 
-cloudinary.config({
-    cloud_name: process.env.CLOUDINARY_NAME,
-    api_key: process.env.CLOUDINARY_KEY,
-    api_secret: process.env.CLOUDINARY_SECRET,
-});
+image.configImageCloud(); //set cloud credentials
 
 const bufferParser = new DatauriParser();
 
 const storage = multer.memoryStorage({});
 const upload = multer({ storage: storage });
 
-router.post("/avatar/", upload.single("image"), (req, res) => {
+router.post("/avatar/", upload.single("image"), async (req, res) => {
     const buffer = req.file.buffer;
     datastring = bufferParser.format(
+        //convert buffer to datauri
         path.extname(req.file.originalname).toString(),
         buffer
     );
-
-    const response = cloudinary.v2.uploader.upload(
-        datastring.content,
-        { folder: "avatar" },
-        (error, result) => {
-            if (!error) {
+    try {
+        const response = await cloudinary.v2.uploader.upload(
+            datastring.content,
+            image.avatarConfig,
+            (error, result) => {
                 res.json(result);
             }
-            res.json(error);
-        }
-    );
+        );
+    } catch (err) {
+        res.json({ message: err });
+    }
 });
-// cloudinary.v2.uploader.upload(req.file.path);
 
-// const image = {};
-// image.url = req.file.url;
-// image.id = req.file.public_id;
-// console.log(image);
-// console.log(req);
-// res.json(image);
-// });
-
-// router.post("/avatar/", parser.avatarParser().single("image"), (req, res) => {
-//     // res.json(req.file);
-//     console.log(req.file);
-//     // cloudinary.v2.uploader.upload(req.file.buffer).then((result) => {
-//     //     const image = result.url;
-//     //     res.json("url", image);
-//     // });
-//     res.json(req.file);
-// });
 module.exports = router;
--- image.js
+++ image.js
@@ -1,3 +1,24 @@
+/****a toolkit about image upload and storage*******/
+
 const multer = require("multer");
 const cloudinary = require("cloudinary");
 const cloudinaryStorage = require("multer-storage-cloudinary");
+
+//configure cloud credentials
+function configImageCloud() {
+    cloudinary.config({
+        cloud_name: process.env.CLOUDINARY_NAME,
+        api_key: process.env.CLOUDINARY_KEY,
+        api_secret: process.env.CLOUDINARY_SECRET,
+    });
+}
+
+//Avatar image transformation
+const avatarTrans = {
+    folder: "avatar",
+    allowedFormats: ["jpg", "png"],
+    transformation: [{ width: 500, height: 500, crop: "limit" }],
+};
+
+module.exports.avatarTrans = avatarTrans;
+module.exports.configImageCloud = configImageCloud;
