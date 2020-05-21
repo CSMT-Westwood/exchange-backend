25411f30d970cacaaf1cb67f6c3f7d0ba6bee4b0
Yiyang <yiyangzhou123@ucla.edu>
2020-05-21 11:10:19 -0700

    bug fixed

--- feed.js
+++ feed.js
@@ -12,38 +12,41 @@ router.get(
     [loginRequired, middlewares.getUserObject],
     async (req, res) => {
         user = req.user;
-        let post;
+        let posts = [];
         //get posts by searching preferences
         let preferencePosts = [];
         for (item of user.preferences) {
-            post = await Post.find(
+            posts = posts.concat((Post.find(
                 { $text: { $search: item } },
                 { score: { $meta: "textScore" } }
-            ).sort({ score: { $meta: "textScore" } });
-            preferencePosts.concat(post);
+            ).sort({ score: { $meta: "textScore" } })));
         }
-
+        preferencePosts = (await Promise.all(posts)).flat();
+        posts = [];
         //get followedPosts
         let followedPosts = [];
         for (item of user.followedPosts) {
-            post = await Post.findOne({ _id: item });
-            if (post !== null) {
-                followedPosts.push(post);
-            }
+            posts.push(Post.findOne({ _id: item }));
         }
+        followedPosts = (await Promise.all(posts)).filter((v) => v !== null);
+        posts = [];
         //get the user's own posts
         let ownPosts = [];
+        console.log(user.posts);
         for (item of user.posts) {
-            post = await Post.findOne({ _id: item });
-            if (post !== null) ownPosts.push(post);
+            posts.push(Post.findOne({ _id: item }));
         }
+
+        ownPosts = (await Promise.all(posts)).filter((v) => v !== null);
+
+        posts = [];
 
         //get posts that the user responded to
         let activities = [];
         for (item of user.activities) {
-            post = await Post.findOne({ _id: item });
-            if (post !== null) activities.push(post);
+            posts.push(Post.findOne({ _id: item }));
         }
+        activities = (await Promise.all(posts)).filter((v) => v !== null);
 
         res.json({ preferencePosts, followedPosts, ownPosts, activities });
     }
@@ -56,32 +59,31 @@ router.get(
     [loginRequired, middlewares.getUserObject],
     async (req, res) => {
         //get the user's own posts
-        let post;
+        let posts = [];
         let ownPosts = {};
         ownPosts.unfulfilled = [];
         ownPosts.pending = [];
         ownPosts.fulfilled = [];
         //get unfulfilled
         for (item of user.posts) {
-            post = await Post.findOne({ _id: item, fulfilled: 0 });
-            if (post !== null) {
-                ownPosts.unfulfilled.push(post);
-            }
+            posts.push(Post.findOne({ _id: item, fulfilled: 0 }));
         }
+
+        ownPosts.unfulfilled = (await Promise.all(posts)).filter((v) => v !== null);
+        posts = [];
+
         //get pending
         for (item of user.posts) {
-            post = await Post.findOne({ _id: item, fulfilled: 1 });
-            if (post !== null) {
-                ownPosts.pending.push(post);
-            }
+            posts.push(Post.findOne({ _id: item, fulfilled: 1 }));
         }
+        ownPosts.pending = (await Promise.all(posts)).filter((v) => v !== null);
+        posts = [];
+
         //get fulfilled
         for (item of user.posts) {
-            post = await Post.findOne({ _id: item, fulfilled: 2 });
-            if (post !== null) {
-                ownPosts.fulfilled.push(post);
-            }
+            posts.push(Post.findOne({ _id: item, fulfilled: 2 }));
         }
+        ownPosts.fulfilled = (await Promise.all(posts)).filter((v) => v !== null);
 
         return res.json(ownPosts);
     }
@@ -94,33 +96,29 @@ router.get(
     [loginRequired, middlewares.getUserObject],
     async (req, res) => {
         //get the user's own posts
-        let post;
+        let posts = [];
         let activities = {};
         activities.unfulfilled = [];
         activities.pending = [];
         activities.fulfilled = [];
         //get unfulfilled
         for (item of user.activities) {
-            post = await Post.findOne({ _id: item, fulfilled: 0 });
-            if (post !== null) {
-                activities.unfulfilled.push(post);
-            }
+            posts.push(Post.findOne({ _id: item, fulfilled: 0 }));
         }
+        activities.unfulfilled = (await (Promise.all(posts))).filter((v) => v !== null);
+        posts = [];
         //get pending
         for (item of user.activities) {
-            post = await Post.findOne({ _id: item, fulfilled: 1 });
-            if (post !== null) {
-                activities.pending.push(post);
-            }
+            posts.push(Post.findOne({ _id: item, fulfilled: 1 }));
         }
+        activities.pending = (await Promise.all(posts)).filter((v) => v !== null);
+
+        posts = [];
         //get fulfilled
         for (item of user.activities) {
-            post = await Post.findOne({ _id: item, fulfilled: 2 });
-            if (post !== null) {
-                activities.fulfilled.push(post);
-            }
+            post.push(Post.findOne({ _id: item, fulfilled: 2 }));
         }
-
+        activities.fulfilled = (await Promise.all(posts)).filter((v) => v !== null);
         return res.json(activities);
     }
 );
@@ -132,32 +130,28 @@ router.get(
     [loginRequired, middlewares.getUserObject],
     async (req, res) => {
         //get the user's own posts
-        let post;
+        let posts = [];
         let followedPosts = {};
         followedPosts.unfulfilled = [];
         followedPosts.pending = [];
         followedPosts.fulfilled = [];
         //get unfulfilled
         for (item of user.followedPosts) {
-            post = await Post.findOne({ _id: item, fulfilled: 0 });
-            if (post !== null) {
-                followedPosts.unfulfilled.push(post);
-            }
+            posts.push(Post.findOne({ _id: item, fulfilled: 0 }));
         }
+        followedPosts.unfulfilled = (await Promise.all(posts)).filter((v) => v !== null);
+        posts = [];
         //get pending
         for (item of user.followedPosts) {
-            post = await Post.findOne({ _id: item, fulfilled: 1 });
-            if (post !== null) {
-                followedPosts.pending.push(post);
-            }
+            posts.push(Post.findOne({ _id: item, fulfilled: 1 }));
         }
+        followedPosts.pending = (await Promise.all(posts)).filter((v) => v !== null);
+        posts = [];
         //get fulfilled
         for (item of user.followedPosts) {
-            post = await Post.findOne({ _id: item, fulfilled: 2 });
-            if (post !== null) {
-                followedPosts.fulfilled.push(post);
-            }
+            post.push(Post.findOne({ _id: item, fulfilled: 2 }));
         }
+        followedPosts.fulfilled = (await Promise.all(posts)).filter((v) => v !== null);
 
         return res.json(followedPosts);
     }
